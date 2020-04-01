e9b615db78e7cbab2f7fea8df0ac635caffd4ed1
Bailey Day <bailey.j.day@gmail.com>
2020-04-01 10:50:39 -0400

    Completed preliminary exercise steps, set up scaffolding for application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    textInput: "",
+    stuffITyped: []
+  };
+
+  handleChange = event => {
+    this.setState({
+      textInput: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const previousInput = this.state.textInput;
+
+    this.setState({
+      textInput: "",
+      stuffITyped: [...this.state.stuffITyped, previousInput]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            Github Username
+            <input
+              type="text"
+              value={this.state.textInput}
+              placeholder="Enter Username"
+              onChange={this.handleChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
